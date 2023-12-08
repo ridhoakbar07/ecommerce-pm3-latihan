cea8b30e41827cc4c48d2331f5889d9958748d46
Ridho Akbar <ridhoakbar.id@gmail.com>
2023-12-08 13:14:29 +0800

    fix verifyLogin kehapus

--- Models/User.php
+++ Models/User.php
@@ -10,5 +10,25 @@ class User extends Model
     }
     
     //method - method lainnya
+    public function getUserByEmail($email)
+    {
+        $query = "SELECT * FROM {$this->table} WHERE email = :email";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function verifyLogin($email, $password)
+    {
+        $user = $this->getUserByEmail($email);
+        if ($user && password_verify($password, $user['password'])) {
+            // Password matches
+            return $user;
+        } else {
+            // Invalid credentials
+            return false;
+        }
+    }
 
 }
